7f239a66fd7f4d80506fbb628ba1c45d67298ad6
Pizzel <220584@umons.ac.be>
2025-03-07 22:16:31 +0100

    feat: added parser using pest

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,44 @@
 use turingrs::{
-    turing_machine::{TuringMachine, TuringMachineExecutor},
-    turing_state::{TuringDirection, TuringTransition},
+    test_parser::test, turing_machine::{TuringMachine, TuringMachineExecutor}, turing_state::{TuringDirection, TuringTransition}
 };
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
 
-    let turing_mach: TuringMachine = TuringMachine::new(2)
-        .add_rule_state(
-            "q_0".to_string(),
-            TuringTransition::new(
-                vec!['ç', 'ç', 'ç'],
-                TuringDirection::Right,
-                vec![('ç', TuringDirection::Right), ('ç', TuringDirection::Right)],
-            ),
-            "q_1".to_string(),
-        )
-        .unwrap()
-        .add_rule_state(
-            "q_1".to_string(),
-            TuringTransition::new(
-                vec!['1', '_', '_'],
-                TuringDirection::Right,
-                vec![('1', TuringDirection::Right), ('0', TuringDirection::Right)],
-            ),
-            "q_a".to_string(),
-        )
-        .unwrap();
+    // let turing_mach: TuringMachine = TuringMachine::new(2)
+    //     .add_rule_state(
+    //         "q_0".to_string(),
+    //         TuringTransition::new(
+    //             vec!['ç', 'ç', 'ç'],
+    //             TuringDirection::Right,
+    //             vec![('ç', TuringDirection::Right), ('ç', TuringDirection::Right)],
+    //         ),
+    //         "q_1".to_string(),
+    //     )
+    //     .unwrap()
+    //     .add_rule_state(
+    //         "q_1".to_string(),
+    //         TuringTransition::new(
+    //             vec!['1', '_', '_'],
+    //             TuringDirection::Right,
+    //             vec![('1', TuringDirection::Right), ('0', TuringDirection::Right)],
+    //         ),
+    //         "q_a".to_string(),
+    //     )
+    //     .unwrap();
 
-    let mut tm_exec =
-        TuringMachineExecutor::new(&turing_mach, "10001110011101".to_string())
-            .unwrap();
+    // let mut tm_exec =
+    //     TuringMachineExecutor::new(&turing_mach, "10001110011101".to_string())
+    //         .unwrap();
 
-    let mut i = 0;
-    println!("Before all : \n{}", tm_exec);
+    // let mut i = 0;
+    // println!("Before all : \n{}", tm_exec);
 
-    for () in &mut tm_exec {
-        println!("{i}");
-        i+=1;
-    }
+    // for () in &mut tm_exec {
+    //     println!("{i}");
+    //     i+=1;
+    // }
  
-    println!("After all : \n{}", tm_exec);
-   
+    // println!("After all : \n{}", tm_exec);
+    test();
 }
