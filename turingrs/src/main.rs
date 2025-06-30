a7f3d3a8fa0073c5fa9a35a863dc8a1642b4c65e
adrien <221212@umons.ac.be>
2025-06-30 22:39:15 +0200

    add workspace

--- turingrs/src/main.rs
+++ turingrs/src/main.rs
@@ -1,9 +1,9 @@
-use ritm::{
+use turingrs::{
     parser::*, turing_machine::{TuringExecutor, TuringMachine, TuringMachineExecutor, TuringMachineExecutorRef}, turing_state::{TuringDirection, TuringTransition}
 };
 
 fn main() {
-    let mut mt = parse_turing_machine_file("resources/turing2.tm".to_string()).unwrap();
+    let mut mt = parse_turing_machine_file("turingrs/resources/turing2.tm".to_string()).unwrap();
     // let mut mt  = parse_turing_machine("".to_string()).unwrap();
     
     // FIX ME This is not okay !
