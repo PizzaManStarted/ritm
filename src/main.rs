57088fc163432158155208dbc45fa446902847cc
Pizzel <220584@umons.ac.be>
2025-06-30 19:15:55 +0200

    rename: changed project name

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use turingrs::{
+use ritm::{
     parser::*, turing_machine::{TuringExecutor, TuringMachine, TuringMachineExecutor, TuringMachineExecutorRef}, turing_state::{TuringDirection, TuringTransition}
 };
 
